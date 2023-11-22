e5b80e9131d9f4cec274d78cef1d6918844cd26f
jarenSg <jarencesar127@gmail.com>
2023-11-22 00:54:50 -0500

    PC3 - SIFUENTES - FINALIZADO

--- src/main/java/com/centroinformacion/service/EditorialService.java
+++ src/main/java/com/centroinformacion/service/EditorialService.java
@@ -1,5 +1,6 @@
 package com.centroinformacion.service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,7 +20,9 @@ public interface EditorialService {
 	
 	
 	public abstract List<Editorial> listaPorRucIgual(String ruc, int idEditorial);
-	
+
+	public abstract List<Editorial> listaConsultaEditorial(int estado, int idPais, String razonSocial,String direccion,
+    String ruc, Date fecDesde, Date fecHasta);
 	
 
 }
